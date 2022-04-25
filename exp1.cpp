2e3263aa478c7210ba5ed42dad0e168b5fbd1744
Abhi-tech-09 <64980572+Abhi-tech-09@users.noreply.github.com>
2022-04-25 15:37:22 +0530

    Abhishek Lex

--- exp1.cpp
+++ exp1.cpp
@@ -1,31 +1,15 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include <bits/stdc++.h>
+using namespace std ; 
+#include <fstream>
+#include <sstream>
 
-//#define endl "\n"
-#define pb push_back
-#define ll long long int
-#define f(i, a, b) for(ll i=a;i<b;i++)
-#define rf(i, a, b) for(ll i=a;i>=b;i--)
 
-#define put(x) cout<<"I am here at "<<x<<" :";
-#define shov(v) for(auto i: v) cout<<i<<" ";cout<<endl;
-#define shom(m) for(auto i: m) cout<<i.first<<" "<<i.second<<endl;
-#define test(t) ll t;cin>>t;while(t--)
-#define __lcm(a, b) (((a) * (b)) / __gcd((a) , (b)))
-
-#define vi vector<int>
-#define vll vector<long long int>
-#define vs vector<string>
-#define mii map<int, int>
-#define si set<int>
-
-vector<string> delimiters = {" ",";", ",", "(", ")", "[", "]", "{", "}"};
-vector<string> operators = {"+", "-", "*", "/", ">", "<", "="};
-vector<string> keywords = {"if", "else", "while", "do", "break", "continue", "int", "float", "double", "return", "char", "case", "sizeof", "long", "short", "typedef", "switch", "unsigned", "void", "static", "struch", "goto", "bool"};
-
+set<string> delimiters = {";", ",", "(", ")", "[", "]", "{", "}" , ">>" , "<<"};
+set<string> operators = {"+", "-", "*", "/", ">", "<", "=" };
+set<string> keywords = {"if", "else", "while", "do", "break", "continue", "int", "float", "double", "return", "char", "case", "sizeof", "long", "short", "typedef", "switch", "unsigned", "void", "static", "struch", "goto", "bool" , "cout" , "cin" , "endl"};
 
 bool isInteger(string s) {
-    f(i, 0, s.length()) {
+    for(int i = 0 ; i < s.size() ; i++) {
         if(int(s[i])<48 || int(s[i]>57)) {
             if(i==0 && s[i]=='-') continue; 
             else return false; 
@@ -34,39 +18,67 @@ bool isInteger(string s) {
     return true;
 }
 
-bool isValid(string s) {
-    char temp = s[0];
-    if(temp>64&&temp<91 || temp>96&&temp<123) return true;
-    else return false;
+set<string>sp; 
+set<string>op; 
+set<string>id; 
+set<string>key; 
+set<string>con;
+
+int main(){
+    ifstream in ; 
+    in.open("source.txt"); 
+    string s ; 
+    while(getline(in , s)) {
+        stringstream x(s); 
+        vector<string>temp;
+        string word ;
+        while(getline(x,word,' ')) temp.push_back(word) ; 
+
+        for(auto lex : temp){
+            if(delimiters.find(lex) != delimiters.end()){
+                sp.insert(lex);
+            }
+            else if(operators.find(lex) != operators.end()){
+                op.insert(lex);
+            }
+            else if(keywords.find(lex) != keywords.end()){
+                key.insert(lex);
+            }
+            else if(isInteger(lex)){
+                con.insert(lex);
+            }
+            else{
+                if(lex[0] != '#' && lex[0] != '<' && lex.back() != ')' && lex != "namespace" && lex != "std" && lex != "using")
+                    id.insert(lex);
+            }
+
+        }
+    }
+
+    for(auto it : op) cout << it << " " ; 
+    cout << endl ; 
+    for(auto it : id) cout << it << " " ; 
+    cout << endl ; 
+    for(auto it : key) cout << it << " " ; 
+    cout << endl ; 
+    for(auto it : con) cout << it << " " ; 
+    cout << endl ; 
+    for(auto it : sp) cout << it << " " ; 
+    cout << endl ; 
+    return 0 ; 
 }
 
-void solve() {
-    map<string, string>m;
-    for(auto i: delimiters) m[i] = "delimiter";
-    for(auto i: operators) m[i] = "operator";
-    for(auto i: keywords) m[i] = "keyword";
-    ll n;cin>>n;
-    vs input(n);
-    f(i, 0, n) cin>>input[i];
-    cout<<"INPUT STRING : ";
-    shov(input);
-    map<string ,vs>result;
-    f(i, 0, n) {
-        if(m.find(input[i]) != m.end()) result[m[input[i]]].pb(input[i]);
-        else if(isInteger(input[i])) result["constants"].pb(input[i]);
-        else if(isValid(input[i])) result["valid identifiers"].pb(input[i]);
-        else result["invalid identifiers"].pb(input[i]);
-    }
-    for(auto i : result) {
-        cout<<i.first<<" : ";
-        shov(i.second);
-    }
-}
+
+
+//Source code
+/*
+#include <bits/stdc++.h>
+using namespace std ; 
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    test(t) solve();
+    int a = 5 ; 
+    int b = a + 2 ; 
+    int c = a + b ; 
+    cout << c << endl ; 
 }
+*/
